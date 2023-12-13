d022bf96f43ce7e45ebb2b6cb782b8e49853fb04
Michal Josef Špaček <tupinek@gmail.com>
2023-12-13 17:14:16 +0100

    Update author name

--- Dicom.pm
+++ Dicom.pm
@@ -77,13 +77,13 @@ L<https://github.com/michal-josef-spacek/Task-Dicom>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2015-2016 Michal Špaček
+ © 2015-2016 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
